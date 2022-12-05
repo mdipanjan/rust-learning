e87abf7a4267a5d4141d54f603441f0e905ab3d1
Dipanjan Mondal <mdipanjan33@gmail.com>
2022-12-05 20:13:52 +0530

    guessing game completed

--- lesson2/guessing_gmae/src/main.rs
+++ lesson2/guessing_gmae/src/main.rs
@@ -1,11 +1,26 @@
-use std::io;
-
+use colored::*;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 fn main() {
     println!("Guess the number");
-    println!("Please input your guess");
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("This is a random number: {}", secret_number);
 
-    io::stdin().read_line(&mut guess).expect("Failed");
+    loop {
+        println!("Please input your guess");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin().read_line(&mut guess).expect("Failed");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("{}", "Too Small...".red()),
+            Ordering::Greater => print!("{}", "Too Big...".red()),
+            Ordering::Equal => {
+                print!("{}", "You win...".green());
+                break;
+            }
+        }
+    }
 }
